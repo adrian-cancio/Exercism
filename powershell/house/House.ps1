056b8eaa3412d7f020f1a0b45919d91b17779a00
Adrián Cancio <ujiktl@gmail.com>
2024-01-08 19:44:22 +0100

    Implement Powershell House Exercise

--- powershell/house/House.ps1
+++ powershell/house/House.ps1
@@ -26,5 +26,42 @@ Function Get-Rhyme() {
         [int]$End
     )
     
-    Throw "Please implement this function"
+    $Subjects = @(
+        "This is the house",
+        "This is the malt",
+        "This is the rat",
+        "This is the cat",
+        "This is the dog",
+        "This is the cow with the crumpled horn",
+        "This is the maiden all forlorn",
+        "This is the man all tattered and torn",
+        "This is the priest all shaven and shorn",
+        "This is the rooster that crowed in the morn",
+        "This is the farmer sowing his corn",
+        "This is the horse and the hound and the horn"
+    )
+
+    $Modifyers = @(
+        "that Jack built",
+        "that lay in the house",
+        "that ate the malt",
+        "that killed the rat",
+        "that worried the cat",
+        "that tossed the dog",
+        "that milked the cow with the crumpled horn"
+        "that kissed the maiden all forlorn",
+        "that married the man all tattered and torn",
+        "that woke the priest all shaven and shorn",
+        "that kept the rooster that crowed in the morn",
+        "that belonged to the farmer sowing his corn"
+    )
+
+    $Sentences=@()
+    
+    for ($i = $Start-1; $i -lt $End; $i++){
+        $Phrase = "$($Subjects[$i]) $($Modifyers[$i..0] -join " ")."
+        $Sentences += $Phrase 
+    }
+
+    return $Sentences -join "`n"
 }

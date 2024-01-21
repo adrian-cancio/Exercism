2424f0f3752e35239452dd40ce2edaf95db23655
Adrián Cancio <ujiktl@gmail.com>
2024-01-21 22:59:38 +0100

    Implement rotational cipher function on PowerShell

--- powershell/rotational-cipher/RotationalCipher.ps1
+++ powershell/rotational-cipher/RotationalCipher.ps1
@@ -20,6 +20,30 @@ Function Invoke-RotationalCipher() {
         [string]$Text, 
         [int]$Shift
     )
-    
-    Throw "Please implement this function"
+
+    [char]$CHAR_a = "a"
+    [char]$CHAR_A = "A"
+    $LETTERS_COUNT = 26
+
+    $ROTDictShift = @{}
+
+    foreach ($Letter in "A".."Z"){
+        $ROTDictShift[[char]$Letter] = [char](
+            ($Letter - $CHAR_A + $Shift) % $LETTERS_COUNT + $CHAR_A
+        )
+    }
+    [String]$ROTShift = ""
+
+    foreach ($Char in $Text.ToCharArray()){
+        $ROTChar = $ROTDictShift[$Char]
+        $ROTCharLower = ([string]$ROTDictShift[[char](([string]$Char).ToUpper())]).ToLower()
+        $ResultChar =
+        if ($ROTChar) { $ROTChar }
+        elseif ($ROTCharLower) { $ROTCharLower }
+        else { $Char }
+
+        $ROTShift += $ResultChar
+    }
+
+    return $ROTShift
 }

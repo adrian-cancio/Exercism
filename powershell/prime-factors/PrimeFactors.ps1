1ccf5a93dae763bae1d997915c7f7d71cf55fa5c
Adrián Cancio <ujiktl@gmail.com>
2024-01-22 23:05:38 +0100

    Added my Prime Factoors PowerShell exercise solution

--- powershell/prime-factors/PrimeFactors.ps1
+++ powershell/prime-factors/PrimeFactors.ps1
@@ -1,3 +1,36 @@
+function Invoke-IsPrime {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)]
+        [Int64]$Number   
+    )
+    
+    if ($Number -eq 2) { return $true }
+    if ($Number -eq 4) { return $false }
+    for ($Candidate = 2; $Candidate -lt $Number/2; $Candidate++){
+        if ($Number % $Candidate -eq 0){
+            return $false;
+        }
+    }
+
+    return $true;
+}
+
+# Non used but useful so I left it
+function Invoke-PrimeNumbers {
+    [CmdletBinding()]
+    param (
+        [Parameter(Position=0)]
+        [ValidateScript({$_ -gt 1}, ErrorMessage="'From' value must be greater than 1.")]
+        [Int64]$From=2,
+        [Parameter(Mandatory, Position=1)]
+        [ValidateScript({$_ -gt $From}, ErrorMessage="'To' value must be greater than 'From' value.")]
+        [Int64]$To
+    )
+    
+    return ($From..$To) | Where-Object {Invoke-IsPrime $_}
+    
+}
 Function Invoke-PrimeFactors() {
     <#
     .SYNOPSIS
@@ -18,5 +51,24 @@ Function Invoke-PrimeFactors() {
         [Int64]$Number
     )
     
-    throw "Please implement this function"
+    [Int64]$Multiple = $Number
+    $PrimeFactors = @()
+
+    while ($Multiple -gt 1) {
+        if (Invoke-IsPrime $Multiple) {
+            $PrimeFactors += $Multiple
+            break
+        }
+        for ($i = 2; $i -le $Multiple/2; $i++){
+            if (($Multiple % $i -eq 0) -and (Invoke-IsPrime $i)){
+                $Multiple /= $i
+                $PrimeFactors += $i
+                break
+            }
+        }
+    }
+
+    return $PrimeFactors
+
+
 }

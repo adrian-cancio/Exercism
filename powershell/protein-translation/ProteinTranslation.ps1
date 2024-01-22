f4cf8968ef821365f0818a15487d4a0e1ad55544
Adrián Cancio <ujiktl@gmail.com>
2024-01-22 05:49:22 +0100

    Implement protein translation exercise in PowerShell

--- powershell/protein-translation/ProteinTranslation.ps1
+++ powershell/protein-translation/ProteinTranslation.ps1
@@ -16,6 +16,45 @@ Function Invoke-ProteinTranslation() {
     Param(
         [string]$Strand
     )
+
+    $InvalidCodonException = "error: Invalid codon"
+
+
+    $Protein2Codon = @{
+    "Methionine" = @("AUG")             
+    "Phenylalanine" = @("UUU", "UUC")
+    "Leucine" = @("UUA", "UUG")
+    "Serine" = @("UCU", "UCC", "UCA", "UCG")
+    "Tyrosine" = @("UAU", "UAC")
+    "Cysteine" = @("UGU", "UGC")
+    "Tryptophan" = @("UGG")
+    "STOP" = @("UAA", "UAG", "UGA")
+    }
+
+    $Codon2Protein = @{}
+    foreach ($Protein in $Protein2Codon.Keys){
+        $Codons = $Protein2Codon[$Protein]
+        foreach ($Codon in $Codons){
+            $Codon2Protein[$Codon] = $Protein
+        }
+    }
+
+    $Translation = @()
+
+    for ($i = 0; $i -lt $Strand.Length; $i+=3){
+        # Used Math Min method to avoid Substring error
+        $Codon = $Strand.Substring($i,[Math]::Min($Strand.Length-$i,3))
+        $Protein = $Codon2Protein[$Codon]
+        if (!$Protein -or $Codon.Length -ne 3){
+            Throw $InvalidCodonException
+        }
+        elseif ($Protein -eq "STOP"){
+            break
+        }
+        $Translation += $Protein
+    }
+
+    return $Translation
     
-    throw "Please implement this function"
+
 }
